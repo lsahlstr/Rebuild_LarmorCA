d7401e1b93df1141f14686d931d792589542a3d1
Logan S. Ahlstrom <lsahlstr@gmail.com>
2016-07-22 06:38:11 -0700

    determined k, k+/-1, k+/-2 in features.py

--- features.py
+++ features.py
@@ -118,7 +118,7 @@ def Features(features, responses, resnums, cs_info, nucleus, xcoors, ycoors, zco
             # Compute geometric features if necessary coordinates exist and i is 
             # more than 6 residues away from the first residue (for determining j)
             # and more than 6 residues away from the last residue (for determining k)
-            if flag == 1 and (i > resnums[5] and i < resnums[-6]):
+            if flag == 1 and (i > resnums[8] and i < resnums[-9]):
     
                 # Feature 1: Distance => i, i-5
                 f1 = abs(sqrt((x-xminus5)**2 + (y-yminus5)**2 + (z-zminus5)**2))
@@ -154,128 +154,146 @@ def Features(features, responses, resnums, cs_info, nucleus, xcoors, ycoors, zco
                 features_res_tmp = (f1,f2,f3,f4,f5,f6,f7,f8,f9,f10)
                                 
                 #### j: closest residue to i that is at least 6 residues FORWARD in sequence ####
-                f21 = 0
                 closest_res = 0  # WILL NEED TO STORE THIS FOR DETERMINING j+1, j+2, ETC.
                 min_dist = 999999
-                if i < resnums[-9]:  # i must be more than 6 residues from the last residue, plus two additional for j+2
-                    for j in range(i+6,resnums[-3]): # need to stop two early so can call j+2
-                        
-                        xj = xcoors.get(j, {}).get('CA')
-                        yj = ycoors.get(j, {}).get('CA')
-                        zj = zcoors.get(j, {}).get('CA')
-                        
-                        dist = abs(sqrt((x-xj)**2 + (y-yj)**2 + (z-zj)**2))
-                        if dist < min_dist:
-                            min_dist = dist
-                            closest_res = j                    
-                    
-                    # j
-                    j = closest_res  # rename closest_res to j for clarity below
+                for j in range(i+6,resnums[-3]): # need to stop two early so can call j+2
                     
                     xj = xcoors.get(j, {}).get('CA')
                     yj = ycoors.get(j, {}).get('CA')
                     zj = zcoors.get(j, {}).get('CA')
                     
-                    # j-1
-                    xj_minus1 = xcoors.get(j-1, {}).get('CA')
-                    yj_minus1 = ycoors.get(j-1, {}).get('CA')
-                    zj_minus1 = zcoors.get(j-1, {}).get('CA')
-                    
-                    # j+1
-                    xj_plus1 = xcoors.get(j+1, {}).get('CA')
-                    yj_plus1 = ycoors.get(j+1, {}).get('CA')
-                    zj_plus1 = zcoors.get(j+1, {}).get('CA')
-                    
-                    # j-2
-                    xj_minus2 = xcoors.get(j-2, {}).get('CA')
-                    yj_minus2 = ycoors.get(j-2, {}).get('CA')
-                    zj_minus2 = zcoors.get(j-2, {}).get('CA')
-                    
-                    # j+2
-                    xj_plus2 = xcoors.get(j+2, {}).get('CA')
-                    yj_plus2 = ycoors.get(j+2, {}).get('CA')
-                    zj_plus2 = zcoors.get(j+2, {}).get('CA')
-                    
-                    # Feature 14: Distance => i-1, j-2
-                    f14 = abs(sqrt((xminus1-xj_minus2)**2 + (yminus1-yj_minus2)**2 + (zminus1-zj_minus2)**2))
-                    
-                    # Feature 15: Distance => i-1, j-1
-                    f15 = abs(sqrt((xminus1-xj_minus1)**2 + (yminus1-yj_minus1)**2 + (zminus1-zj_minus1)**2))
-                    
-                    # Feature 16: Distance => i-1, j
-                    f16 = abs(sqrt((xminus1-xj)**2 + (yminus1-yj)**2 + (zminus1-zj)**2))
-                    
-                    # Feature 17: Distance => i-1, j+1
-                    f17 = abs(sqrt((xminus1-xj_plus1)**2 + (yminus1-yj_plus1)**2 + (zminus1-zj_plus1)**2))
-                    
-                    # Feature 18: Distance => i-1, j+2
-                    f18 = abs(sqrt((xminus1-xj_plus2)**2 + (yminus1-yj_plus2)**2 + (zminus1-zj_plus2)**2))
-                    
-                    # Feature 19: Distance => i, j-2
-                    f19 = abs(sqrt((x-xj_minus2)**2 + (y-yj_minus2)**2 + (z-zj_minus2)**2))
-                    
-                    # Feature 20: Distance => i, j-1
-                    f20 = abs(sqrt((x-xj_minus1)**2 + (y-yj_minus1)**2 + (z-zj_minus1)**2))
-                    
-                    # Feature 21: Distance => i,j (min_dist above)
-                    f21 = min_dist
-                    
-                    # Feature 22: Distance => i, j+1
-                    f22 = abs(sqrt((x-xj_plus1)**2 + (y-yj_plus1)**2 + (z-zj_plus1)**2))
-                    
-                    # Feature 23: Distance => i, j+2
-                    f23 = abs(sqrt((x-xj_plus2)**2 + (y-yj_plus2)**2 + (z-zj_plus2)**2))
-                    
-                    # Feature 24: Distance => i+1, j-2
-                    f24 = abs(sqrt((xplus1-xj_minus2)**2 + (yplus1-yj_minus2)**2 + (zplus1-zj_minus2)**2))
-                    
-                    # Feature 25: Distance => i+1, j-1
-                    f25 = abs(sqrt((xplus1-xj_minus1)**2 + (yplus1-yj_minus1)**2 + (zplus1-zj_minus1)**2))
-                    
-                    # Feature 26: Distance => i+1, j
-                    f26 = abs(sqrt((xplus1-xj)**2 + (yplus1-yj)**2 + (zplus1-zj)**2))
-                    
-                    # Feature 27: Distance => i+1, j+1
-                    f27 = abs(sqrt((xplus1-xj_plus1)**2 + (yplus1-yj_plus1)**2 + (zplus1-zj_plus1)**2))
-                    
-                    # Feature 28: Distance => i+1, j+2
-                    f28 = abs(sqrt((xplus1-xj_plus2)**2 + (yplus1-yj_plus2)**2 + (zplus1-zj_plus2)**2))  
+                    dist = abs(sqrt((x-xj)**2 + (y-yj)**2 + (z-zj)**2))
+                    if dist < min_dist:
+                        min_dist = dist
+                        closest_res = j                    
                 
+                # j
+                j = closest_res  # rename closest_res to j for clarity below
+                
+                xj = xcoors.get(j, {}).get('CA')
+                yj = ycoors.get(j, {}).get('CA')
+                zj = zcoors.get(j, {}).get('CA')
+                
+                # j-1
+                xj_minus1 = xcoors.get(j-1, {}).get('CA')
+                yj_minus1 = ycoors.get(j-1, {}).get('CA')
+                zj_minus1 = zcoors.get(j-1, {}).get('CA')
+                
+                # j+1
+                xj_plus1 = xcoors.get(j+1, {}).get('CA')
+                yj_plus1 = ycoors.get(j+1, {}).get('CA')
+                zj_plus1 = zcoors.get(j+1, {}).get('CA')
+                
+                # j-2
+                xj_minus2 = xcoors.get(j-2, {}).get('CA')
+                yj_minus2 = ycoors.get(j-2, {}).get('CA')
+                zj_minus2 = zcoors.get(j-2, {}).get('CA')
+                
+                # j+2
+                xj_plus2 = xcoors.get(j+2, {}).get('CA')
+                yj_plus2 = ycoors.get(j+2, {}).get('CA')
+                zj_plus2 = zcoors.get(j+2, {}).get('CA')
+                
+                # Feature 14: Distance => i-1, j-2
+                f14 = abs(sqrt((xminus1-xj_minus2)**2 + (yminus1-yj_minus2)**2 + (zminus1-zj_minus2)**2))
+                
+                # Feature 15: Distance => i-1, j-1
+                f15 = abs(sqrt((xminus1-xj_minus1)**2 + (yminus1-yj_minus1)**2 + (zminus1-zj_minus1)**2))
+                
+                # Feature 16: Distance => i-1, j
+                f16 = abs(sqrt((xminus1-xj)**2 + (yminus1-yj)**2 + (zminus1-zj)**2))
+                
+                # Feature 17: Distance => i-1, j+1
+                f17 = abs(sqrt((xminus1-xj_plus1)**2 + (yminus1-yj_plus1)**2 + (zminus1-zj_plus1)**2))
+                
+                # Feature 18: Distance => i-1, j+2
+                f18 = abs(sqrt((xminus1-xj_plus2)**2 + (yminus1-yj_plus2)**2 + (zminus1-zj_plus2)**2))
+                
+                # Feature 19: Distance => i, j-2
+                f19 = abs(sqrt((x-xj_minus2)**2 + (y-yj_minus2)**2 + (z-zj_minus2)**2))
+                
+                # Feature 20: Distance => i, j-1
+                f20 = abs(sqrt((x-xj_minus1)**2 + (y-yj_minus1)**2 + (z-zj_minus1)**2))
+                
+                # Feature 21: Distance => i,j (min_dist above)
+                f21 = min_dist
+                
+                # Feature 22: Distance => i, j+1
+                f22 = abs(sqrt((x-xj_plus1)**2 + (y-yj_plus1)**2 + (z-zj_plus1)**2))
+                
+                # Feature 23: Distance => i, j+2
+                f23 = abs(sqrt((x-xj_plus2)**2 + (y-yj_plus2)**2 + (z-zj_plus2)**2))
+                
+                # Feature 24: Distance => i+1, j-2
+                f24 = abs(sqrt((xplus1-xj_minus2)**2 + (yplus1-yj_minus2)**2 + (zplus1-zj_minus2)**2))
+                
+                # Feature 25: Distance => i+1, j-1
+                f25 = abs(sqrt((xplus1-xj_minus1)**2 + (yplus1-yj_minus1)**2 + (zplus1-zj_minus1)**2))
+                
+                # Feature 26: Distance => i+1, j
+                f26 = abs(sqrt((xplus1-xj)**2 + (yplus1-yj)**2 + (zplus1-zj)**2))
+                
+                # Feature 27: Distance => i+1, j+1
+                f27 = abs(sqrt((xplus1-xj_plus1)**2 + (yplus1-yj_plus1)**2 + (zplus1-zj_plus1)**2))
+                
+                # Feature 28: Distance => i+1, j+2
+                f28 = abs(sqrt((xplus1-xj_plus2)**2 + (yplus1-yj_plus2)**2 + (zplus1-zj_plus2)**2))
+
                 # Store features that depend on the ith and jth residues
                 features_res_tmp = features_res_tmp + (f14,f15,f16,f17,f18,f19,f20,f21,
-                										f22,f23,f24,f25,f26,f27,f28)
+                                                        f22,f23,f24,f25,f26,f27,f28)
                                         
                 #### k: closest residue to i that is at least 6 residues BACKWARDS in sequence ####
-                f36 = 0
-                closest_res = 0  # reset min_res; WILL NEED TO STORE THIS FOR DETERMINING k+1, k+2, ETC.
+                closest_res = 0   # reset min_res; WILL NEED TO STORE THIS FOR DETERMINING k+1, k+2, ETC.
                 min_dist = 999999 # reset min_dist
-                if i > resnums[5]:  # i must be more than 6 residues from the first residue
-                    #print 'i = ' + str(i)
+                for k in range(i-6,resnums[2]-1,-1):  # count backwards (-1 for decrement); have to go to resnum[2]-1
+            
+                    xk = xcoors.get(k, {}).get('CA')
+                    yk = ycoors.get(k, {}).get('CA')
+                    zk = zcoors.get(k, {}).get('CA')
+            
+                    dist = abs(sqrt((x-xk)**2 + (y-yk)**2 + (z-zk)**2))
+                    if dist < min_dist:
+                        min_dist = dist
+                        closest_res = k 
                 
-                    for k in range(i-6,resnums[0]-1,-1):  # count backwards (-1 for decrement); have to go to resnum[0]-1
-                        #print k
-                    
-                        xk = xcoors.get(k, {}).get('CA')
-                        yk = ycoors.get(k, {}).get('CA')
-                        zk = zcoors.get(k, {}).get('CA')
-                    
-                        dist = abs(sqrt((x-xk)**2 + (y-yk)**2 + (z-zk)**2))
-                        #print dist
-                        if dist < min_dist:
-                            min_dist = dist
-                            closest_res = k
-                            
-                        #print 'min dist: ' + str(min_dist)
-                    
-                    f36 = min_dist
+                # k
+                k = closest_res  # rename closest_res to k for clarity below
                 
-                #features_res_tmp = (f21,f36)
-                #features_res_tmp = features_res_tmp + (f36,)
-                #features_res_tmp = (f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f20,f21,f22,f36)
+                xk = xcoors.get(k, {}).get('CA')
+                yk = ycoors.get(k, {}).get('CA')
+                zk = zcoors.get(k, {}).get('CA')
+                
+                # k-1
+                xk_minus1 = xcoors.get(k-1, {}).get('CA')
+                yk_minus1 = ycoors.get(k-1, {}).get('CA')
+                zk_minus1 = zcoors.get(k-1, {}).get('CA')
+                
+                # k+1
+                xk_plus1 = xcoors.get(k+1, {}).get('CA')
+                yk_plus1 = ycoors.get(k+1, {}).get('CA')
+                zk_plus1 = zcoors.get(k+1, {}).get('CA')
+                
+                # k-2
+                xk_minus2 = xcoors.get(k-2, {}).get('CA')
+                yk_minus2 = ycoors.get(k-2, {}).get('CA')
+                zk_minus2 = zcoors.get(k-2, {}).get('CA')
+                
+                # k+2
+                xk_plus2 = xcoors.get(k+2, {}).get('CA')
+                yk_plus2 = ycoors.get(k+2, {}).get('CA')
+                zk_plus2 = zcoors.get(k+2, {}).get('CA')
+                                     
+                # Feature 36: Distance => i,k (min_dist above)
+                f36 = min_dist
+                
+                features_res_tmp = features_res_tmp + (f36,)  
+                
                 
                 features_tmp.append(features_res_tmp)
                 responses_tmp.append(cs)
-    	#print ""
+
+        
     # For residue i features, combine with features for i+1 and i-1 
     #responses = responses_tmp[1:len(responses)-1]
     for i in range(1,len(features_tmp)-1):
